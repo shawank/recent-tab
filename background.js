98932fff58f4072771d9b93b801ebc3069021733
Ankush Shaw <shawank17198@gmail.com>
2025-03-11 00:18:16 +0530

    Removed usage tracking code

--- background.js
+++ background.js
@@ -8,8 +8,6 @@ var altPressed = false;
 var wPressed = false;
 
 var domLoaded = false
-var quickActive = 0;
-var slowActive = 0;
 
 var prevTimestamp = 0;
 var slowtimerValue = 1500;
@@ -282,51 +280,3 @@ var generatePrintMRUString = function() {
 }
 
 initialize();
-
-var quickSwitchActiveUsage = function() {
-
-	if(domLoaded) {
-		if(quickActive == -1) {
-			return;
-		} else if(quickActive < 5){
-			quickActive++;
-		} else if(quickActive >= 5) {
-			_gaq.push(['_trackEvent', 'activeUsage', 'quick']);
-			quickActive = -1;
-		}
-	}
-}
-
-var slowSwitchActiveUsage = function() {
-
-	if(domLoaded) {
-		if(slowActive == -1) {
-			return;
-		} else if(slowActive < 5){
-			slowActive++;
-		} else if(slowActive >= 5) {
-			_gaq.push(['_trackEvent', 'activeUsage', 'slow']);
-			slowActive = -1;
-		}
-	}
-}
-
-//check for actual active users
-var _AnalyticsCode = 'UA-51920707-1';
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', _AnalyticsCode]);
-_gaq.push(['_trackPageview']);
-
-(function() {
-	var ga = document.createElement('script');
-	ga.type = 'text/javascript';
-	ga.async = true;
-	ga.src = 'https://ssl.google-analytics.com/ga.js';
-	//ga.src = 'https://ssl.google-analytics.com/u/ga_debug.js';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(ga, s);
-})();
-document.addEventListener('DOMContentLoaded', function () {
-	domLoaded = true;
-
-});
